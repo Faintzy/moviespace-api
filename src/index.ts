52c517ac298657a689fa2c49df6e3c5bf8f21aa2
Gabriel (Faintzy) <gabrielmrts@yahoo.com>
2021-02-08 16:09:34 -0300

    Arquivo principal

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('ok');
+import "reflect-metadata";
+import * as bodyParser from "body-parser";
+import * as cors from "cors";
+import * as express from "express";
+import routes from "./routes";
+import { Connection, createConnection } from "typeorm";
+
+const app = express();
+
+createConnection()
+.then(Connection => {
+    app.use(bodyParser.json());
+    app.use(cors());
+    app.use(routes);
+
+    app.listen(3033);
+});
