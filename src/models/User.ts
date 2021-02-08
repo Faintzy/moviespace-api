108ffd7aba818f1cac625809e87a653c8dd90300
Gabriel (Faintzy) <gabrielmrts@yahoo.com>
2021-02-08 16:11:52 -0300

    Model do usuário

--- src/models/User.ts
+++ src/models/User.ts
@@ -7,12 +7,17 @@ export class User {
     id: number;
 
     @Column()
-    firstName: string;
+    email: string;
 
     @Column()
-    lastName: string;
+    username: string;
 
     @Column()
-    age: number;
+    password: string;
+
+    @Column({
+        default: "[]"
+    })
+    favorites: string;
 
 }
